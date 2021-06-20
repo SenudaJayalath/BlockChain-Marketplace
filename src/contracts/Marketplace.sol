fe006474511f3cdf1496a17c76f177923fb6179d
SenudaJayalath <37454346+SenudaJayalath@users.noreply.github.com>
2021-06-20 16:49:42 +0530

    ADD buying functionality to smart contract

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -28,7 +28,8 @@ contract Marketplace {
 
     function createProduct(string memory _name, uint256 _price) public {
         // Make sure the paramaeters are correct
-
+        require(bytes(_name).length > 0);
+        require(_price > 0);
         //Increment product count
         productCount++;
         // Create the product
@@ -41,5 +42,22 @@ contract Marketplace {
         );
         // Trigger an event
         emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
+
+    function purchaseProduct(uint256 _id) public {
+        //Fetch the product
+        Product memory _product = products[_id];
+        //Fetch the owner
+        address _seller = _product.owner;
+        //Make sure the product is valid
+        //Transfer ownership
+        _product.owner = msg.sender;
+        //Mark as purchased
+        _product.purchased = true;
+        //Update the product
+        products[_id] = _product;
+        //Pay the seller by sending Ether
+
+        //Trigger an event
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,8 +1,12 @@
 const { assert } = require("chai")
 
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
 const Marketplace =artifacts.require('Marketplace.sol')
 
-contract('Marketplace',(accounts)=>{
+contract('Marketplace',([deployer,seller,buyer])=>{
     let marketplace
 
     before( async () => {
@@ -24,16 +28,38 @@ contract('Marketplace',(accounts)=>{
         })
     })
 
-    describe ('product', async ()=>{
+    describe('product', async ()=>{
         let result,productCount
         before( async ()=>{
-            result = await marketplace.createProduct('iPhone X',web3.utils.toWei('1','Ether'))
+            result = await marketplace.createProduct('iPhone X',web3.utils.toWei('1','Ether'),{from: seller})
             productCount= await marketplace.productCount()
         })
         it('create products', async ()=>{
             //SUCCESS
             assert.equal(productCount,1)
-            console.log(result.logs)
+            const event =result.logs[0].args
+            assert.equal(event.id.toNumber(),productCount.toNumber(),'id is correct')
+            assert.equal(event.name ,'iPhone X')
+            assert.equal(event.price ,'1000000000000000000')
+            assert.equal(event.owner ,seller)
+            assert.equal(event.purchased ,false)
+
+            //FAILURES
+
+            await marketplace.createProduct('',web3.utils.toWei('1','Ether'),{from: seller}).should.be.rejected
+            await marketplace.createProduct('iPhone X','',{from: seller}).should.be.rejected
+        })
+
+        it('lists products', async ()=>{
+            const product =await marketplace.products(productCount)
+            assert.equal(product.id.toNumber(),productCount.toNumber(),'id is correct')
+            assert.equal(product.name ,'iPhone X')
+            assert.equal(product.price ,'1000000000000000000')
+            assert.equal(product.owner ,seller)
+            assert.equal(product.purchased ,false)
+        })
+        it('sells products', async ()=>{
+            const product =await marketplace.purchaseProduct(productCount,{from:buyer, value: web3.utils.toWei('1','Ether')})
         })
     })
 
